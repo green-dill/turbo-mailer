37611173d02434ce06a6c1a715ce924a006a2410
yinheli <me@yinheli.com>
2024-11-10 21:42:11 +0800

    feat: add rabbitmq heartbeat and tls

--- turbo-mailer-server/internal/rabbitmq/rabbitmq.go
+++ turbo-mailer-server/internal/rabbitmq/rabbitmq.go
@@ -1,12 +1,29 @@
 package rabbitmq
 
 import (
+	"crypto/tls"
+	"os"
+	"time"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/spf13/viper"
 )
 
 func Dial() (*amqp.Connection, error) {
-	return amqp.Dial(viper.GetString("rabbitmq.url"))
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+
+	return amqp.DialConfig(viper.GetString("rabbitmq.url"), amqp.Config{
+		Heartbeat: 10 * time.Second,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		Properties: amqp.Table{
+			"connection_name": hostname,
+		},
+	})
 }
 
 func Channel(conn *amqp.Connection) (*amqp.Channel, error) {
