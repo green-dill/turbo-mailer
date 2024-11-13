060be1bf8f8519a7406a7613f2e27c0224443f2f
yinheli <me@yinheli.com>
2024-11-13 23:53:08 +0800

    feat: add render function

--- turbo-mailer-server/internal/render/render.go
+++ turbo-mailer-server/internal/render/render.go
@@ -23,6 +23,9 @@ var (
 		"rand":          rand.Intn,
 		"randFloat":     rand.Float64,
 		"now":           now,
+		"dict":          dict,
+		"slice":         slice,
+		"append":        _append,
 		"randomPickOne": randomPickOne,
 	}
 )
@@ -35,24 +38,47 @@ func Render(content string, params any) (string, error) {
 		return "", err
 	}
 
+	if params == nil {
+		params = map[string]any{}
+	}
+
 	buf := &strings.Builder{}
 	if err := tmpl.Execute(buf, params); err != nil {
 		return "", err
 	}
-	return buf.String(), nil
+	return strings.TrimSpace(buf.String()), nil
 }
 
 func toTitle(s string) string {
 	return cases.Title(language.English).String(s)
 }
 
-func randomPickOne(items []string) string {
+func randomPickOne(items []any) any {
 	if len(items) == 0 {
-		return ""
+		return nil
 	}
 	return items[rand.Intn(len(items))]
 }
 
 func now() string {
 	return time.Now().Format(time.ANSIC)
+}
+
+func dict(values ...any) map[string]any {
+	if len(values)%2 != 0 {
+		return nil
+	}
+	d := make(map[string]any, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		d[values[i].(string)] = values[i+1]
+	}
+	return d
+}
+
+func slice(items ...any) []any {
+	return items
+}
+
+func _append(slice []any, items ...any) []any {
+	return append(slice, items...)
 }
